d97e368cf4a3b97e3cfd8299f2c8efd67c078296
bashy yacob <bashy3309@gmail.com>
2025-01-27 18:18:30 +0200

    Update titles in db.json and modify navigation buttons in Dashboard and HomePage components

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -19,15 +19,6 @@ const HomePage = ({ user }) => {
 
   return (
     <div className="home-page">
-      <header className="home-header">
-        <nav className="home-nav">
-          <button onClick={() => navigate(`/users/${userId}/home/info`)} className="home-nav-button">Info</button>
-          <button onClick={() => navigate(`/users/${userId}/todos`)} className="home-nav-button">Todos</button>
-          <button onClick={() => navigate(`/users/${userId}/posts`)} className="home-nav-button">Posts</button>
-          <button onClick={() => navigate(`/users/${userId}/albums`)} className="home-nav-button">Albums</button>
-          <button onClick={handleLogout} className="home-nav-button logout-button">Logout</button>
-        </nav>
-      </header>
       <h1>Welcome, {user?.name}!</h1>
       <main className="home-content">
         <p>Select an option from the menu to get started!</p>
